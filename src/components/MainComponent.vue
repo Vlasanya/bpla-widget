2cef9958c069f4fc15e94807856a3a842789a40e
“Vlasanya” <anyavlasenko@gmail.com>
2024-05-14 14:30:35 +0200

    change path3

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -25,7 +25,7 @@ const backgroundImage = new Image();
 backgroundImage.src = '@/map_image.png';
 
 const droneImage = new Image();
-droneImage.src = '/assets/drone.svg';
+droneImage.src = '@/assets/drone.svg';
 
 let timePerStep = 160; 
 
